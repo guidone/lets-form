6d86edf9704821b63375d7b5e442a877a9a4c3e3
guidone <guido.bellomo@gmail.com>
2023-03-29 10:47:03 +0200

    build phase

---
+++ scripts/copy-all-files.sh
@@ -0,0 +1,14 @@
+echo "Copy files from source repo..."
+cp -avR ../form-builder/src/components/lets-form/assets/* ./assets
+cp -avR ../form-builder/src/components/lets-form/common/* ./common
+cp -avR ../form-builder/src/components/lets-form/components/* ./components
+cp -avR ../form-builder/src/components/lets-form/generator/* ./generator
+cp -avR ../form-builder/src/components/lets-form/helpers/* ./helpers
+cp -av ../form-builder/src/components/lets-form/costants.js ./costants.js
+cp -avR ../form-builder/src/components/lets-form/index.js ./index.js
+cp -avR ../form-builder/src/components/lets-form/assets/* ./icons
+cp -avR ../form-builder/src/components/lets-form/react/* ./react
+cp -avR ../form-builder/src/components/lets-form/react-antd/* ./react-antd
+cp -avR ../form-builder/src/components/lets-form/react-bootstrap/* ./react-bootstrap
+cp -avR ../form-builder/src/components/lets-form/react-material-ui/* ./react-material-ui
+cp -avR ../form-builder/src/components/lets-form/react-rsuite5/* ./react-rsuite5
