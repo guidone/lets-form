0b31568598779449e3fe96ac9612787b7542fea8
guidone <guido.bellomo@gmail.com>
2024-03-06 22:52:57 +0100

    improve helpers

--- helpers/pass-rest.js
+++ helpers/pass-rest.js
@@ -1,3 +1,20 @@
 import _ from 'lodash';
 
-export const passRest = props => _.omit(props, 'lfFramework', 'lfComponent', 'lfLocale', 'lfOnEnter', 'transformer', 'script');
+const EXCLUDED_LETSFORM_FIELDS = [
+  'lfFramework', 
+  'lfComponent', 
+  'lfLocale', 
+  'lfOnEnter', 
+  'transformer', 
+  'script'
+];
+
+/**
+ * passRest
+ * Help method to filter props passed with the rest props, removing lets-form specific properties
+ * @param {*} props 
+ * @param {*} additional 
+ * @returns 
+ */
+export const passRest = (props, additional = []) => 
+  _.omit(props, [...EXCLUDED_LETSFORM_FIELDS, ...(_.isArray(additional) ? additional : [additional])]);
