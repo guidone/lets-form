4e3f86417824103e86aa3428add6c018dbb9091d
guidone <guido.bellomo@gmail.com>
2024-09-06 16:23:26 +0200

    feat: rsuite definitions

--- react-rsuite5/index.d.ts
+++ react-rsuite5/index.d.ts
@@ -1,81 +1,34 @@
 
 import { ChangeEvent, FunctionComponent } from 'react';
 
-type Locale = (
-  | "af-ZA"
-  | "ar"
-  | "bg-BG"
-  | "ca-AD"
-  | "cs-CZ"
-  | "cy-GB"
-  | "da-DK"
-  | "de-DE"
-  | "el-GR"
-  | "en-US"
-  | "es-ES"
-  | "et-EE"
-  | "eu"
-  | "fa-IR"
-  | "fi-FI"
-  | "fr-FR"
-  | "he-IL"
-  | "hi-IN"
-  | "hr-HR"
-  | "hu-HU"
-  | "id-ID"
-  | "is-IS"
-  | "it-IT"
-  | "ja-JP"
-  | "km-KH"
-  | "ko-KR"
-  | "la"
-  | "lt-LT"
-  | "lv-LV"
-  | "mn-MN"
-  | "nb-NO"
-  | "nl-NL"
-  | "nn-NO"
-  | "pl-PL"
-  | "pt-PT"
-  | "ro-RO"
-  | "ru-RU"
-  | "sk-SK"
-  | "sl-SI"
-  | "sr-RS"
-  | "sv-SE"
-  | "th-TH"
-  | "tr-TR"
-  | "uk-UA"
-  | "vi-VN"
-  | "zh-CN"
-);
 
-type ObjectOfStrings = {
-  [index: string]: any;
-}
+import {
+  LfFieldReactRsuite5Props,
+  LfColumnsReactRsuite5Props,
+  LfColumnReactRsuite5Props,
+  LfTabsReactRsuite5Props,
+  LfTabReactRsuite5Props,
+  LfStepsReactRsuite5Props,
+  LfStepReactRsuite5Props,
+  LfGroupReactRsuite5Props,
+  LfArrayReactRsuite5Props,
+  LetsFormReactRsuite5Props,
+  LetsFormSchemaReactRsuite5
+} from "../../../manifest/lets-form-manifests/dist/definitions/react-rsuite5";
 
-interface LetsFormProps {
-  name?: string;
-  locale?: Locale;
-  plaintext?: boolean;
-  debug?: boolean;
-  disabled?: boolean;
-  disableButtons?: boolean;
-  form?: object;
-  demo?: boolean;
-  context?: object;
-  defaultValues?: ObjectOfStrings;
-  onChange?: (values: ObjectOfStrings) => void
-}
 
-interface LfFieldProps {
-  component: string;
-  name: string;
-  required?: boolean;
-  label?: string;
-}
 
-const LetsForm: FunctionComponent<LetsFormProps>;
-const LfField: FunctionComponent<LfFieldProps>
 
+declare const LetsForm: FunctionComponent<LetsFormReactRsuite5Props>;
 export default LetsForm;
+
+export const LfField: FunctionComponent<LfFieldReactRsuite5Props>;
+export const LfColumns: FunctionComponent<LfColumnsReactRsuite5Props>;
+export const LfColumn: FunctionComponent<LfColumnReactRsuite5Props>;
+export const LfTabs: FunctionComponent<LfTabsReactRsuite5Props>;
+export const LfTab: FunctionComponent<LfTabReactRsuite5Props>;
+export const LfSteps: FunctionComponent<LfStepsReactRsuite5Props>;
+export const LfStep: FunctionComponent<LfStepReactRsuite5Props>;
+export const LfGroup: FunctionComponent<LfGroupReactRsuite5Props>;
+export const LfArray: FunctionComponent<LfArrayReactRsuite5Props>;
+export { LetsFormSchemaReactRsuite5 };
