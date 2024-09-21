6b1d7d49ec9499a20715c92e20d94d8bb2cea574
guidone <guido.bellomo@gmail.com>
2024-09-21 12:57:29 +0200

    expose type script stuff

---
+++ react-antd.d.ts
@@ -0,0 +1,29 @@
+import { FunctionComponent } from 'react';
+
+import {
+  LfFieldReactAntdProps,
+  LfColumnsReactAntdProps,
+  LfColumnReactAntdProps,
+  LfTabsReactAntdProps,
+  LfTabReactAntdProps,
+  LfStepsReactAntdProps,
+  LfStepReactAntdProps,
+  LfGroupReactAntdProps,
+  LfArrayReactAntdProps,
+  LetsFormReactAntdProps,
+  LetsFormSchemaReactAntd
+} from './definitions/react-antd';
+
+declare const LetsForm: FunctionComponent<LetsFormReactAntdProps>;
+export default LetsForm;
+
+export const LfField: FunctionComponent<LfFieldReactAntdProps>;
+export const LfColumns: FunctionComponent<LfColumnsReactAntdProps>;
+export const LfColumn: FunctionComponent<LfColumnReactAntdProps>;
+export const LfTabs: FunctionComponent<LfTabsReactAntdProps>;
+export const LfTab: FunctionComponent<LfTabReactAntdProps>;
+export const LfSteps: FunctionComponent<LfStepsReactAntdProps>;
+export const LfStep: FunctionComponent<LfStepReactAntdProps>;
+export const LfGroup: FunctionComponent<LfGroupReactAntdProps>;
+export const LfArray: FunctionComponent<LfArrayReactAntdProps>;
+export { LetsFormSchemaReactAntd };
---
+++ react-rsuite5.d.ts
@@ -0,0 +1,29 @@
+import { FunctionComponent } from 'react';
+
+import {
+  LfFieldReactRsuite5Props,
+  LfColumnsReactRsuite5Props,
+  LfColumnReactRsuite5Props,
+  LfTabsReactRsuite5Props,
+  LfTabReactRsuite5Props,
+  LfStepsReactRsuite5Props,
+  LfStepReactRsuite5Props,
+  LfGroupReactRsuite5Props,
+  LfArrayReactRsuite5Props,
+  LetsFormReactRsuite5Props,
+  LetsFormSchemaReactRsuite5
+} from './definitions/react-rsuite5';
+
+declare const LetsForm: FunctionComponent<LetsFormReactRsuite5Props>;
+export default LetsForm;
+
+export const LfField: FunctionComponent<LfFieldReactRsuite5Props>;
+export const LfColumns: FunctionComponent<LfColumnsReactRsuite5Props>;
+export const LfColumn: FunctionComponent<LfColumnReactRsuite5Props>;
+export const LfTabs: FunctionComponent<LfTabsReactRsuite5Props>;
+export const LfTab: FunctionComponent<LfTabReactRsuite5Props>;
+export const LfSteps: FunctionComponent<LfStepsReactRsuite5Props>;
+export const LfStep: FunctionComponent<LfStepReactRsuite5Props>;
+export const LfGroup: FunctionComponent<LfGroupReactRsuite5Props>;
+export const LfArray: FunctionComponent<LfArrayReactRsuite5Props>;
+export { LetsFormSchemaReactRsuite5 };
