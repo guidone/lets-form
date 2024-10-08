3daf8685cb5f5e2487fe129a3598be41cf36f8d8
guidone <guido.bellomo@gmail.com>
2024-10-08 18:46:20 +0200

    add fields on top

--- helpers/add-field.js
+++ helpers/add-field.js
@@ -4,24 +4,25 @@ import { mapFields } from './map-fields';
 
 /**
  * addField
- * @param {*} form The form to add the field to 
+ * @param {*} form The form to add the field to
  * @param {*} newField The new field { component: '', ... } or array of fields
  * @param {*} id Id of the component to add the field to
  * @param {*} target the fields key of the components: "fields", "tabs", "leftFields", "rightFields", ...
  * @param {*} subtarget the index of the target in case it's an array
- * @returns 
+ * @returns
  */
 const addField = (
-  form, 
-  newField, 
-  id, 
+  form,
+  newField,
+  id,
   target = 'fields',
-  subtarget = null
+  subtarget = null,
+  position = 'end'
 ) => {
   // can add multiple fields at once
   const toAdd = _.isArray(newField) ? newField : [newField];
 
-  if (id != null) {    
+  if (id != null) {
     return {
       ...form,
       fields: mapFields(
@@ -29,25 +30,34 @@ const addField = (
         // if right field id, append to fields
         field => {
           if (field.id === id) {
+            // sub target is for columns, steps, tabs
             if (subtarget != null) {
+              const what = position === 'start' ?
+                [
+                  ...toAdd,
+                  ...(field[target] && field[target][subtarget] ? field[target][subtarget] : []),
+                ] :
+                [
+                  ...(field[target] && field[target][subtarget] ? field[target][subtarget] : []),
+                  ...toAdd
+                ];
+
               return {
                 ...field,
                 [target]: {
                   ...(field[target] || []),
-                  [subtarget]: [
-                    ...(field[target] && field[target][subtarget] ? field[target][subtarget] : []),
-                    ...toAdd
-                  ]
+                  [subtarget]: what
                 }
               };
             } else {
-              // old way
+              // only target is for groups
+              const what = position === 'start' ?
+                [...toAdd, ...(field[target] || [])] :
+                [...(field[target] || []), ...toAdd];
+
               return {
                 ...field,
-                [target]: [
-                  ...(field[target] || []),
-                  ...toAdd
-                ]
+                [target]: what
               };
             }
           }
@@ -55,6 +65,7 @@ const addField = (
         })
     };
   } else {
+    // just add in the main "fields" key
     return {
       ...form,
       fields: [
@@ -65,4 +76,31 @@ const addField = (
   }
 }
 
-export { addField };
+/**
+ * AddFieldAfter
+ * Add a field (an object with "component" property to a form) after the field with a specific id.
+ * Returns a form
+ * @param {*} form The form to add the field to
+ * @param {*} newField The new field { component: '', ... } or array of fields
+ * @param {*} id Id of the component to add the field after
+ */
+const AddFieldAfter = (form, newField, id) => {
+
+  const toAdd = _.isArray(newField) ? newField : [newField];
+  return {
+    ...form,
+    fields: mapFields(
+      form.fields,
+      // if right field id, append to fields
+      field => {
+        if (field.id === id) {
+
+          return [field, ...toAdd];
+        }
+        return field;
+      }
+    )
+  };
+};
+
+export { addField, AddFieldAfter };
